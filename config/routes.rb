34db5eecf8afbdb6e832db2c291e89eb73b9e970
Diana Pavel <dianapav@gmail.com>
2019-09-17 14:36:11 -0300

    added the home page root

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   get 'venues/show'
   get 'venues/update'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'venues#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
