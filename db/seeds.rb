5cea65d159b45fdbd7ee978e650babc20d219122
Anderson Costa <andersonlzcosta@gmail.com>
2019-09-17 11:22:14 -0300

    four locations but no details

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Venue.destroy_all
+
+Venue.create!(name: 'Cervantes', location: 'Copacabana, Rio de Janeiro')
+Venue.create!(name: 'Lapa 40', location: 'Lapa, Rio de Janeiro')
+Venue.create!(name: 'Sugar Loaf', location: 'Urca, Rio de Janeiro')
+Venue.create!(name: 'Sinuca da Bambina', location: 'Botafogo, Rio de Janeiro')
