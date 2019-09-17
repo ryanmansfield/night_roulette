141d6ce6cc8e39a103a691aad860561b4a817b93
Diana Pavel <dianapav@gmail.com>
2019-09-17 12:35:27 -0300

    deleted again

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'venues/index'
-  get 'venues/show'
-  get 'venues/update'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
