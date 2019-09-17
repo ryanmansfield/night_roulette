4d6d619874f674cb2ac8b0f097bfcae7c7631878
Diana Pavel <dianapav@gmail.com>
2019-09-17 12:37:02 -0300

    generated controller Venues

---
+++ app/controllers/venues_controller.rb
@@ -0,0 +1,10 @@
+class VenuesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+end
---
+++ test/controllers/venues_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class VenuesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get venues_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get venues_show_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get venues_update_url
+    assert_response :success
+  end
+
+end
