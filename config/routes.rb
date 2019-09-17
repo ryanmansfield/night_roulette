60ecbac1cf182c339c7d79119f0d4fd54b0b0b4a
Diana Pavel <dianapav@gmail.com>
2019-09-17 12:34:06 -0300

    deleted the wrong controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   get 'venues/index'
   get 'venues/show'
   get 'venues/update'
-  get 'venue/index'
-  get 'venue/show'
-  get 'venue/update'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
