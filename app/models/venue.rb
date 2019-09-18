c08b6aefa6f2c8d079f3b66097c55c3dc22e46b5
Ryan Mansfield <ryanmansfield86@gmail.com>
2019-09-18 15:44:34 -0300

    Changed the logic of the select_random venues action

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -7,32 +7,10 @@ class Venue < ApplicationRecord
   def self.find_random(type, user)
     # TODO: this will be the 3 bar rest nightclub filters and user will be user preferences
 
-    all_venues = Venue.all
-    possible_venues = []
+    # Find which types were selected from the new bookings form
 
-    # Did the user check restaurant in the form?
-    #if yes does the venue include restaurant in characteristics?
-    # then add this venue to an array of possible venues
+    # Check venues were type match to see if they qualify for the users preferences
 
-      # if form restaurant = true
-
-      all_venues.each do |venue|
-        if characteristics.include?("restaurant")
-          venue << possible_venues
-        end
-      end
-
-      # Did the user check bar in the form?
-      #if yes does the venue include bar in characteristics?
-      # then add this venue to an array of possible venues
-
-      # Did the user check nightclub in the form?
-      # if yes does the venue include nightclub in characteristics?
-      # then add this venue to an array of possible venues
-
-      # check the possible venues array for the users preferences(meat/sports/etc)
-      # return only ones that match user preferences
-      # Select a random location from thi newest array and use that as the venue for the booking
 
     Venue.last
   end
