b00b3cafdeb58038bc26831c532efeda0082d22d
Brittany Correll <brittany@gooverseas.com>
2019-09-18 14:24:10 -0300

    added geocoding for venues

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,4 +3,6 @@ class Venue < ApplicationRecord
   has_many :users, through: :bookings
   validates :name, presence: true, uniqueness: true
   validates :location, presence: true
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
