c78ba4ca48e674fb4872b3a6a4bf5cb349e2059e
Ryan Mansfield <ryanmansfield86@gmail.com>
2019-09-18 14:35:29 -0300

    completed sudo code logic for the find_random method

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -6,13 +6,37 @@ class Venue < ApplicationRecord
 
   def self.find_random(filters, user)
     # TODO: this will be the 3 bar rest nightclub filters and user will be user preferences
-    # is it a bar?
+
+    possible_venues = []
+
+    # Did the user check restaurant in the form?
+
+      #if yes does the venue include restaurant in characteristics?
       venue.characteristics.include?("restaruant")
-    # is it a restaruant
+      # then add this venue to an array of possible venues
+
+
+
+      # Did the user check bar in the form?
+
+      #if yes does the venue include bar in characteristics?
       venue.characteristics.include?("bar")
-    # is it a nightclub
+      # then add this venue to an array of possible venues
+
+
+
+       # Did the user check nightclub in the form?
+
+      #if yes does the venue include nightclub in characteristics?
       venue.characteristics.include?("nightclub")
-    # now cross check all of this with the users preferences
+      # then add this venue to an array of possible venues
+
+
+      # check the possible venues array for the users preferences(meat/sports/etc)
+
+      # return only ones that match user preferences
+
+      # Select a random location from thi newest array and use that as the venue for the booking
 
     Venue.last
   end
