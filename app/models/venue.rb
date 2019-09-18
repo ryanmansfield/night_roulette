6e4d37367ef6f1b3eafdbc1f26aeef9be10aaf37
Ryan Mansfield <ryanmansfield86@gmail.com>
2019-09-18 14:25:54 -0300

    Added some wrong logic to the find_random method

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -7,8 +7,11 @@ class Venue < ApplicationRecord
   def self.find_random(filters, user)
     # TODO: this will be the 3 bar rest nightclub filters and user will be user preferences
     # is it a bar?
+      venue.characteristics.include?("restaruant")
     # is it a restaruant
+      venue.characteristics.include?("bar")
     # is it a nightclub
+      venue.characteristics.include?("nightclub")
     # now cross check all of this with the users preferences
 
     Venue.last
