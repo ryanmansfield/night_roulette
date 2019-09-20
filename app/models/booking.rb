44defaee21d14fb067a22e7928669d4d9f070876
Ryan Mansfield <ryanmansfield86@gmail.com>
2019-09-20 13:52:19 -0300

    Fixed the backend logic

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -14,7 +14,7 @@ class Booking < ApplicationRecord
   private
 
   def assign_random_venue
-    @venue_types = ['restaurant', 'bar', 'nightclub'] if @venue_types[0].empty?
+    @venue_types = ['restaurant', 'bar', 'nightclub'] if @venue_types[1].blank?
     self.venue = Venue.find_random(@venue_types)
     self.time = Time.now
     self.date = Date.today
