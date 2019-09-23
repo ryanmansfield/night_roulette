fd4d513b7f2316fd0d4719e6b123878770bc98a7
Diana Pavel <dianapav@gmail.com>
2019-09-23 15:15:05 -0300

    added cool facts

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,7 @@ Venue.create!(
     location: '205 Rua Cândido Gaffrée, Urca Rio de Janeiro, 22291-080, Brazil',
     picture: 'https://lh5.googleusercontent.com/p/AF1QipPLgmEhwzn_bYyJ2b5zK4Ndf4GWvGhzY-KzL2uU=w426-h240-k-no',
     venue_type: 'bar'
+    cool_facts: ['The food is great', 'Order a caipirinha', 'The views are awesome']
 )
 #2
 Venue.create!(
