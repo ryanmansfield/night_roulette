e85bc4cdf03a50fb4cb8152177ad4e6b2d711feb
Brittany Correll <brittany@gooverseas.com>
2019-09-24 13:49:26 -0300

    fix zoom

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -21,7 +21,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 100, maxZoom: 3 });
+  map.fitBounds(bounds, { padding: 100, maxZoom: 12 });
 };
 
 const initMapbox = () => {
