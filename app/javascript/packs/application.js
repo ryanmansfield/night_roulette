114e1e421e4cb6f4e55e1637e2317af20514246c
Anderson Costa <andersonlzcosta@gmail.com>
2019-09-26 15:46:21 -0300

    js working and some style at the buttons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,16 @@
 import 'bootstrap';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
-import { newBooking } from '../components/new-booking';
+import {
+  newBooking,
+  increasePassengerAmount,
+  decreasePassengerAmount
+} from '../components/new-booking';
 import { copyToClipBoard } from '../components/copy-to-clip-board';
 import { mapRoulette } from '../components/map-roulette';
 initMapbox();
 newBooking();
+increasePassengerAmount();
+decreasePassengerAmount();
 mapRoulette();
 copyToClipBoard();
