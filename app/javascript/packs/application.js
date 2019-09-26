7383ea6019ce4a34b4b131306a1514c5dd5db9db
Anderson Costa <andersonlzcosta@gmail.com>
2019-09-26 00:13:13 -0300

    spinning the pins

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
-import "bootstrap";
-import 'mapbox-gl/dist/mapbox-gl.css'
+import 'bootstrap';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import { newBooking } from '../components/new-booking';
+import { mapRoulette } from '../components/map-roulette';
 initMapbox();
 newBooking();
+mapRoulette();
