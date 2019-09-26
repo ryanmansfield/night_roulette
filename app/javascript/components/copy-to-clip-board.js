3606afc516ff0852a3cfead9311d5fd1646dc3ac
Anderson Costa <andersonlzcosta@gmail.com>
2019-09-26 00:37:45 -0300

    missing curly brackets

--- app/javascript/components/copy-to-clip-board.js
+++ app/javascript/components/copy-to-clip-board.js
@@ -1,12 +1,13 @@
 function copyToClipBoard() {
   /* Get the text field */
-  var copyText = document.getElementById("myInput");
+  var copyText = document.getElementById('myInput');
 
   /* Select the text field */
   copyText.select();
   copyText.setSelectionRange(0, 99999); /*For mobile devices*/
 
   /* Copy the text inside the text field */
-  document.execCommand("copy");
+  document.execCommand('copy');
+}
 
 export { copyToClipBoard };
