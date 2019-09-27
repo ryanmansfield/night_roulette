29eb6cf7ff32eced3db82e450d1af4b52347c706
Anderson Costa <andersonlzcosta@gmail.com>
2019-09-27 11:07:00 -0300

    infinity loop at the spin pins

--- app/javascript/components/map-roulette.js
+++ app/javascript/components/map-roulette.js
@@ -8,17 +8,17 @@ const mapRoulette = () => {
   const spinPins = () => {
     pins.forEach((pin, index, pins) => {
       setTimeout(() => {
-        if (index > 1) {
+        if (index == 0) {
+          blinkPin(pin);
+        } else if (index == pins.length) {
+          blinkPin(pins[index - 1]);
+        } else {
           blinkPin(pin);
           blinkPin(pins[index - 1]);
         }
-        // else if (index == pins.length - 1) {
-        //   setTimeout(() => {
-        //     blinkPin(pin);
-        //   }, 2000 * index);
-        // }
       }, 500 * (1 + index * 0.5));
     });
+    setTimeout(spinPins, 3000);
   };
   const spinner = document.querySelector('#spin-it');
   if (spinner) {
